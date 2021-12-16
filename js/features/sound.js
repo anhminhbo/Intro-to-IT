44a1fc0bd862af5628922f87b3b9af4d484fabe8
Minh Nguyens <minhnguyen@wee.vn>
2021-12-16 12:04:00 +0700

    add sound and img components

---
+++ js/features/sound.js
@@ -0,0 +1,17 @@
+// Ball Hit Wall 
+const BALL_HIT_WALL_SOUND = new Audio();
+BALL_HIT_WALL_SOUND.src = '../../sound/wall.mp3'
+BALL_HIT_WALL_SOUND.volume = 0.2;
+
+
+
+// Ball hit Paddle 
+const BALL_HIT_PADDLE_SOUND = new Audio();
+BALL_HIT_PADDLE_SOUND.src = '../../sound/paddle.mp3'
+BALL_HIT_PADDLE_SOUND.volume = 0.1;
+
+// Game over
+const GAME_OVER_SOUND = new Audio();
+GAME_OVER_SOUND.src = '../../sound/gameover.mp3'
+GAME_OVER_SOUND.volume = 0.2;
+
